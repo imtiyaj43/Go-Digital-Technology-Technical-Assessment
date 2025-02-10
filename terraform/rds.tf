30e5e6bab862d8ae943b518e29d0a3b1e511d8fc
Shaikh Imtiyaj <69413004+imtiyaj43@users.noreply.github.com>
2025-02-10 18:20:27 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,17 @@
-hello
+resource "aws_db_instance" "rds_db" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "godigitaldb"
+  username             = "admin"
+  password             = "Shaikh14"
+  parameter_group_name = "default.mysql8.0"
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+
+  tags = {
+    Name        = "Go Digital RDS Database"
+    Environment = "Dev"
+  }
+}
