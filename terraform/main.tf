fb9e322d4372657b4ac5b37d8d97b666693c12e2
Shaikh Imtiyaj <69413004+imtiyaj43@users.noreply.github.com>
2025-02-10 18:25:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 resource "aws_s3_bucket" "data_bucket" {
-  bucket = "go-digital-data-bucket"
-  acl    = "private"
+  bucket = "my-data-bucket"
+}
 
-  tags = {
-    Name        = "Go Digital Data Bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket_acl" "data_bucket_acl" {
+  bucket = aws_s3_bucket.data_bucket.id
+  acl    = "private"
 }
