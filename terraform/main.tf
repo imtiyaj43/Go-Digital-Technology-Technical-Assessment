7b9889a66425d8de7ad2ffb6e1f06b6e0da6e5ff
Shaikh Imtiyaj <69413004+imtiyaj43@users.noreply.github.com>
2025-02-10 21:52:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,7 @@ resource "random_string" "suffix" {
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "my-data-bucket-${random_string.suffix.result}"
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.your_bucket.id
+}
