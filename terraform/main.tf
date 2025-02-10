4b549d5932fad667f973c59cf87f00eb8ca4058b
Shaikh Imtiyaj <69413004+imtiyaj43@users.noreply.github.com>
2025-02-10 19:00:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-resource "aws_s3_bucket" "data_bucket" {
-  bucket = "my-data-bucket"
-}
-
-resource "aws_s3_bucket_acl" "data_bucket_acl" {
-  bucket = aws_s3_bucket.data_bucket.id
-  acl    = "private"
-}
-
 resource "random_string" "suffix" {
   length  = 6
   special = false
@@ -15,4 +6,9 @@ resource "random_string" "suffix" {
 
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "my-data-bucket-${random_string.suffix.result}"
+}
+
+resource "aws_s3_bucket_acl" "data_bucket_acl" {
+  bucket = aws_s3_bucket.data_bucket.id
+  acl    = "private"
 }
